2b96f5650da70bf7602b7194449093dc4bbbe228
Sylvain75 <sylvoz75@gmail.com>
2017-10-03 20:55:42 +0800

    step 12

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
     res.render('index');
 };
@@ -6,6 +9,15 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    // res.json(req.body);
+    const store =  await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+    // query the DB for a list of all the stores
+    const stores = await Store.find();
+    res.render('stores', { title: 'Stores', stores });
+}
